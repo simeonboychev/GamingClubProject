6d5bede21a0948ba3ec2be356664a5868fd8acb4
Simeon Borislavov Boychev <simeon.boychev98@gmail.com>
2020-02-13 11:34:03 +0200

    Project init

---
+++ GamingProject.Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace GamingProject.Models
+{
+    // Add profile data for application users by adding properties to the User class
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ GamingProject/Utilities/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,75 @@
+﻿using GamingProject.Data.Context;
+using GamingProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamingProject.Utilities.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UpdateDatabase(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<GamingProjectContext>();
+                context.Database.EnsureCreated();
+
+                var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                var userManager = serviceScope.ServiceProvider.GetService<UserManager<User>>();
+
+
+                Task.Run(async () =>
+                {
+                    var adminName = "admin";
+
+                    var adminExists = await roleManager.RoleExistsAsync(adminName);
+
+                    if (!adminExists)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole
+                        {
+                            Name = adminName
+                        });
+                    }
+
+                    var moderatorName = "BarCrawler";
+
+                    var moderatorExists = await roleManager.RoleExistsAsync(moderatorName);
+
+                    if (!moderatorExists)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole
+                        {
+                            Name = moderatorName
+                        });
+                    }
+
+                    var adminUser = await userManager.FindByNameAsync(adminName);
+
+                    if (adminUser == null)
+                    {
+                        adminUser = new User
+                        {
+                            UserName = "admin",
+                            Email = "admin@admin.com",
+                            //Role = 
+
+                        };
+
+                        await userManager.CreateAsync(adminUser, "admin12");
+                        await userManager.AddToRoleAsync(adminUser, adminName);
+                    }
+
+                })
+                .GetAwaiter()
+                .GetResult();
+            }
+
+        }
+    }
+}
